17e3d09da52c66a7ebf71895e6314b3557b129fa
Aziz <abdullaev12@bk.ru>
2023-10-09 22:39:46 +0300

    Initial commit

---
+++ app/src/main/java/com/abdullaev/and_3_1_1/and_3_1/MainActivity.java
@@ -0,0 +1,16 @@
+package com.abdullaev.and_3_1_1.and_3_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.abdullaev.and_3_1.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
